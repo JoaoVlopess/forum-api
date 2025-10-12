e7043c3ad9b1f3b8e3506ff2c132f864f1577dec
João Victor Lopes De Carvalho <joaovlopes.carvalho@gmail.com>
2025-10-12 10:27:36 -0300

    feat: Create CRUD

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,45 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Put } from '@nestjs/common';
 import { User as UserModel} from 'generated/prisma';
 import { UserService } from './user.service';
+import { Prisma } from '@prisma/client';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
 
     constructor(private userService: UserService) {}
 
-  @Post('signup') // requisição de criar um usuario
+  @Post() // requisição de criar um usuario
   async signupUser(
     @Body() userData: { email: string ; name: string; password: string;}, // ou usar o Prisma.UserCreateInput -> simbolizam o tipo do model/tabela criado
   ): Promise<UserModel> {
     return this.userService.createUser(userData); // chama o create user do user.service
   }
+  @Get(':id')
+  async getUser(@Param('id') id: string): Promise<UserModel> {
+    const user = await this.userService.user({ id: Number(id) });
 
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+
+    return user;
+  }
+
+  @Put(':id')
+  async updateUser(
+    @Body() userData: {email: string ; name: string; password: string},
+    @Param('id') id :string
+  ): Promise<UserModel> {
+      return this.userService.updateUser({
+        where: { id: Number(id)},
+        data: userData});
+    }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.deleteUser({id: Number(id)})
+  }
 }
+
+
+
