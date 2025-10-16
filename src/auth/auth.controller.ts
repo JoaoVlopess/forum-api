a167b265fc29c27a630e4852b530d4740b04d6d4
João Victor Lopes De Carvalho <joaovlopes.carvalho@gmail.com>
2025-10-16 13:49:42 -0300

    feat: crate auth user signin

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Inject, Post } from '@nestjs/common';
+import { User } from 'generated/prisma';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    @Inject()
+    private readonly authService: AuthService;
+    
+    @Post('signin')
+    @HttpCode(HttpStatus.OK)
+    signin(@Body() body: { email: string ; password: string;}) {
+        return this.authService.signIn(body);
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import { User } from 'generated/prisma';
+
+import {UserService} from 'src/user/user.service'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(private userService: UserService) {}
+
+  async signIn(params:{ email, password }): Promise <Omit<User, 'password'>> {
+    const user = await this.userService.user({email: params.email});
+    if (!user) throw new NotFoundException('User not found');
+    const passwordMatch = await bcrypt.compare(params.password, user.password);
+    if (!passwordMatch) throw new UnauthorizedException('Invalid credentials');
+
+    const {password,...result} = user;
+    return result;
+
+  }
+}
